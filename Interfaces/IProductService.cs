47d40c26e8f98d7c8fec94ca64d2ed7a9ce1dfe2
ayiamco <josephachonu@gmail.com>
2021-05-04 12:35:35 +0100

    added the product service

---
+++ Interfaces/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using Shopping_Cart.Models;
+using System.Collections.Generic;
+
+namespace Shopping_Cart.Services
+{
+    public interface IProductService
+    {
+        IList<Product> GetData();
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using Shopping_Cart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Shopping_Cart.Services
+{
+    public class ProductService : IProductService
+    {
+        public IList<Product> GetData()
+        {
+            string url = "https://fakestoreapi.com/products";
+            using (var client = new WebClient())
+            {
+                var content = client.DownloadString(url);
+                var list = DeserializeToList<Product>(content);
+                return list;
+            }
+        }
+
+
+        private IList<T> DeserializeToList<T>(string jsonString)
+        {
+            var array = JArray.Parse(jsonString);
+            IList<T> objectsList = new List<T>();
+
+            foreach (var item in array)
+            {
+                try
+                {
+                    // CorrectElements  
+                    objectsList.Add(item.ToObject<T>());
+                }
+                catch
+                {
+
+                }
+            }
+
+            return objectsList;
+        }
+    }
+}
